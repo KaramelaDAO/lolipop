05dc7a235d39eb1d07ff9f7afe9ecdc4f8efd1b8
Jon V <jon@agentrisk.com>
2022-02-14 12:43:35 -0800

    contract first version

---
+++ token-smart-contract/contracts/Lollipop.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+
+contract Lollipop is ERC20, Ownable {
+    uint256 private immutable _cap = 1000000 * 10**decimals();
+
+    constructor() ERC20("Lollipop", "loll"){
+        _mint(msg.sender, cap());
+    }
+
+
+    /**
+     * @dev Returns the cap on the token's total supply.
+     */
+    function cap() public view virtual returns (uint256) {
+        return _cap;
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        require(
+            ERC20.totalSupply() + amount <= cap(),
+            "ERC20Capped: cap exceeded"
+        );
+        _mint(to, amount);
+    }
+}
