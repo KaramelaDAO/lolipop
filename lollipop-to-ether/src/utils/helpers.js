afca6d8297d9a5b7871d674794c34ae880a5538e
George Chatzigiannis <geohatzigiannis@gmail.com>
2022-05-12 19:25:41 +0100

    resolved issues with blocked ui

--- lollipop-to-ether/src/utils/helpers.js
+++ lollipop-to-ether/src/utils/helpers.js
@@ -1,2 +1,4 @@
 export const convertToDecimal = (val, decimalPlaces) =>
   Math.trunc(parseFloat(val) * Math.pow(10,decimalPlaces)) / Math.pow(10,decimalPlaces);
+
+export const isMobileAgent  = () => /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
